e0d1fb63a82c24f460a1a2f715f57633205281bd
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-08-28 22:10:02 +0530

    updated

---
+++ A. Helpful Maths/code.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <bitset>
+#include <iomanip>
+#include <numeric>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+template <typename T>
+void input(vector<T> &arr, int n) {
+     for (int i = 0; i < n; i++) {
+         cin >> arr[i];
+     }
+}
+
+void solve() {
+    string s;
+    cin>>s;
+    vc temp;
+    for(int i=0;i<s.length();i+=2){
+        temp.push_back(s[i]);
+    }
+    sort(temp.begin(),temp.end());
+    for(int i=0;i<temp.size();i++){
+        cout<<temp[i];
+        if(i != temp.size()-1) cout<<"+"; //if i is not the last element then add + after adding every digit
+    }
+    cout<<"\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+    return 0;
+}
