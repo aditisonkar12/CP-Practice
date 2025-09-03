9623db3475f8298059f26cdd37deb933706e43a2
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-09-03 19:24:21 +0530

    updated

---
+++ A. Nearly Lucky Number/code.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <bitset>
+#include <iomanip>
+#include <numeric>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+template <typename T>
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void solve()
+{
+    long long n;
+    cin >> n;
+    int cnt = 0;
+    while (n > 0)
+    {
+        int temp = n % 10;
+        if (temp == 4 || temp == 7)
+            cnt++;
+        n /= 10;
+    }
+    if (cnt == 4 or cnt == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+    return 0;
+}
