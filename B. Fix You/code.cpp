12ffcfaa2f0015a00c8422627dd153b184a831af
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-11-03 20:06:18 +0530

    updated

---
+++ B. Fix You/code.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <bitset>
+#include <iomanip>
+#include <numeric>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+template <typename T>
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> grid(n, vector<char>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (grid[i][m - 1] != 'D') // if last column is not D change it to D expect the bottommost cell
+            cnt++;
+    }
+    for (int j = 0; j < m - 1; j++)
+    {
+        if (grid[n - 1][j] != 'R') // if last column is not R change it to R expect the bottommost row cell
+            cnt++;
+    }
+    cout << cnt << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
