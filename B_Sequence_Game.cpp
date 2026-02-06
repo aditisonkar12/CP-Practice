b07ade4f1fac8cd165a5dd3ad1e1bf88efa5bb95
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-02-06 17:19:56 +0530

    updated

--- B_Sequence_Game.cpp
+++ B_Sequence_Game.cpp
@@ -34,46 +34,55 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
 template <typename T>
-void input(vector<T> &arr, int n) {
-     for (int i = 0; i < n; i++) {
-         cin >> arr[i];
-     }
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 }
 
-void solve() {
+void solve()
+{
     int n;
-    cin>>n;
+    cin >> n;
 
     vll b(n);
-    input (b,n);
+    input(b, n);
 
     vector<long long> a;
     a.push_back(b[0]);
 
-    for(int i=1;i<n;i++){
-        if(b[i]>=b[i-1]){
+    for (int i = 1; i < n; i++)
+    {
+        if (b[i] >= b[i - 1])
+        {
             a.push_back(b[i]);
         }
-        else{
+        else
+        {
             a.push_back(1);
             a.push_back(b[i]);
         }
     }
-    cout<<a.size()<<"\n";
+    cout << a.size() << "\n";
 
-    for(auto x:a){
-        cout<<x<<" ";
+    for (auto x : a)
+    {
+        cout << x << " ";
     }
-    cout<<"\n";
+    cout << "\n";
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
