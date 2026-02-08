30cc40ac290edd446c9d8e49366b4631789134d0
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-02-08 13:43:46 +0530

    updated

--- A_United_We_Stand.cpp
+++ A_United_We_Stand.cpp
@@ -34,47 +34,55 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
 template <typename T>
-void input(vector<T> &arr, int n) {
-     for (int i = 0; i < n; i++) {
-         cin >> arr[i];
-     }
+void input(vector<T> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 }
 
-void solve() {
+void solve()
+{
     int n;
-        cin >> n;
-        vector<long long> a(n);
-        for (auto &x : a) cin >> x;
+    cin >> n;
+    vector<long long> a(n);
+    for (auto &x : a)
+        cin >> x;
 
-        sort(a.begin(), a.end());
+    sort(a.begin(), a.end());
 
-        long long mn = a[0];
-        int cnt = 0;
-        for (auto x : a)
-            if (x == mn) cnt++;
+    long long mn = a[0];
+    int cnt = 0;
+    for (auto x : a)
+        if (x == mn)
+            cnt++;
 
-        if (cnt == n) {
-            cout << -1 << '\n';
-            continue;
-        }
+    if (cnt == n)
+    {
+        cout << -1 << '\n';
+        return;
+    }
 
-        cout << cnt << " " << n - cnt << '\n';
-        for (int i = 0; i < cnt; i++)
-            cout << mn << " ";
-        cout << '\n';
+    cout << cnt << " " << n - cnt << '\n';
+    for (int i = 0; i < cnt; i++)
+        cout << mn << " ";
+    cout << '\n';
 
-        for (int i = cnt; i < n; i++)
-            cout << a[i] << " ";
-        cout << '\n';
+    for (int i = cnt; i < n; i++)
+        cout << a[i] << " ";
+    cout << '\n';
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
