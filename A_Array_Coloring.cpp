dbf7e847c8505fe77026d3fa43dd41e1c66cbf4d
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-03-09 19:06:00 +0530

    updated

--- A_Array_Coloring.cpp
+++ A_Array_Coloring.cpp
@@ -43,6 +43,13 @@ void input(vector<T> &arr, int n) {
 void solve() {
     int n;
     cin>>n;
+    int odd=0;
+
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        
+    }
 }
 
 int main() {
