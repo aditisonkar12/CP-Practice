d9265871d92d3708543ef299b99445e92e743c42
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-03-27 22:35:43 +0530

    updated

--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -59,7 +59,8 @@ void solve()
     }
 
     vector<int> prefixZeroes(n + 1, 0);
-    
+
+
     for (int i = 0; i < n; i++)
     {
         prefixZeroes[i + 1] = prefixZeroes[i] + (s[i] == '0' ? 1 : 0);
