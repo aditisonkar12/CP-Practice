f18ea4a46d54af38f0e7f63a49be9c2a7bb9975d
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-04-02 08:53:34 +0530

    updated

--- B_Monsters.cpp
+++ B_Monsters.cpp
@@ -58,7 +58,7 @@ void solve()
     sort(dq.begin(), dq.end(), [](const pair<long long, long long> &a, const pair<long long, long long> &b)
          { 
         if(a.first != b.first) 
-            return a.first < b.first;
+            return a.first > b.first;
             
         return a.second < b.second; });
 
