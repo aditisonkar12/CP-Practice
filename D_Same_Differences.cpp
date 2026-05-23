c4e1db9099577131b64ebde45a37a4f08532bff8
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2026-05-23 15:48:12 +0530

    updated

--- D_Same_Differences.cpp
+++ D_Same_Differences.cpp
@@ -50,6 +50,13 @@ void solve()
     input(arr, n);
 
     int cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            while(i<j){
+                
+            }
+        }
+    }
 }
 
 int main()
